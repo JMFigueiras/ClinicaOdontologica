41a5189f2573aa6de5473d7680d34525b46a5410
JMFigueiras <jmfigueiras98@gmail.com>
2019-07-08 19:47:16 -0300

    Form edited

--- src/app/Services/dentist-shift.service.ts
+++ src/app/Services/dentist-shift.service.ts
@@ -7,13 +7,39 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class DentistShiftService {
 
-  constructor(private fireStore: AngularFirestore, private authService:AuthService) {}
+	constructor(private fireStore: AngularFirestore, private authService:AuthService) {}
 
 
-  dentistShiftRegister(dni: number, date: string, hour: string){
+	dentistShiftRegister(dni: string, date: string, hour: string, specialist: string): Promise<Object>{
 
-  	let user;
+  	//Validación...
+
+  		const request: Object = {
+	      dni: dni,
+	      date: date,
+	      hour: hour,
+	      specialist: specialist
+	    };
+
+	    return this.fireStore.collection('dentistShifts').add(request);
 
 
   }
+
+    	/*public userRegister(dni: string, email: string, password: string, firstName: string, lastName: string, type: string, file: any): Promise<Object> {
+	    const request: Object = {
+	      dni: dni,
+	      email: email,
+	      password: password,
+	      firstName: firstName,
+	      lastName: lastName,
+	      type: type
+	    };
+
+	    this.authService.signUp(email, password);
+
+	    console.log(this.fireStorage.upload(dni, file));
+
+	    return this.fireStore.collection('users').add(request);
+	}*/
 }
